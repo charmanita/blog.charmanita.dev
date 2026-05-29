17a18d5282143fd221138be7ce46f2c5da35a6bd
charmanita <hdroberson23@gmail.com>
2026-05-29 18:44:13 -0500

    switching to api

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -8,7 +8,7 @@
   <meta property="og:title" content={data.meta.title} />
   <meta
     property="og:image"
-    content={`https://blog.charmanita.dev/og?title=${encodeURIComponent(data.meta.title)}&desc=${encodeURIComponent(data.meta.description ?? "")}`}
+    content={`https://mcapi.charmanita.dev/og?title=${encodeURIComponent(data.meta.title)}&desc=${encodeURIComponent(data.meta.description ?? "")}`}
   />
   <meta property="og:image:width" content="1200" />
   <meta property="og:image:height" content="630" />
