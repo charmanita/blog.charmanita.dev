4c45979d02a30f950600b8a98b9c9b33dd1cda5f
charmanita <hdroberson23@gmail.com>
2026-05-29 19:36:04 -0500

    added more text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 
 <main>
   <div class="container">
-    <a href="https://charmanita.dev" class="back">← back</a>
+    <a href="https://charmanita.dev" class="back">← back to charmanita.dev</a>
     <h1 class="title">blog</h1>
     <div class="posts">
       {#each posts as post}
